37a68f2fe4f11c807417c682596106bbadce5f09
sergiosagripanti@gmail.com <sergiosagripanti@gmail.com@7e0d7668-b102-ec16-678f-7dbf3825cd3e>
2012-07-08 22:20:34 +0000

    Saludo Vegas

--- ProyectoConcurrente/src/Main.java
+++ ProyectoConcurrente/src/Main.java
@@ -7,6 +7,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola sergio!!!!");
+		System.out.println("Hola Vegas!!!!");
 	}
 
 }
