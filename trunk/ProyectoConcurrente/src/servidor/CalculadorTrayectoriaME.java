b36ccb627e4609d52f39f0401c888be213f91317
vegasruffmurphy@gmail.com <vegasruffmurphy@gmail.com@7e0d7668-b102-ec16-678f-7dbf3825cd3e>
2012-07-16 03:01:19 +0000

    Agregue clase CalculadorTrayectoria

---
+++ trunk/ProyectoConcurrente/src/servidor/CalculadorTrayectoriaME.java
@@ -0,0 +1,15 @@
+package servidor;
+
+public class CalculadorTrayectoriaME {
+/*debe analizar si un misil es amenaza o no
+ * si es amenaza lo debe pasar a la clase control para que lo destruya
+ */
+	public void procesar(double pos[],double vec[])
+	{};
+	
+	
+	
+	
+	
+	
+}
