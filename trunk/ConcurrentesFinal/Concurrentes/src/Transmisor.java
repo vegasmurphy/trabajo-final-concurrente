a2f269cbb83169505f94b5d59a63800d5e821795
vegasruffmurphy@gmail.com <vegasruffmurphy@gmail.com@7e0d7668-b102-ec16-678f-7dbf3825cd3e>
2013-07-24 17:50:17 +0000

    se agrego clase Transmisor

---
+++ trunk/ConcurrentesFinal/Concurrentes/src/Transmisor.java
@@ -0,0 +1,78 @@
+import java.net.*;
+
+import java.io.*;
+
+public class Transmisor extends Thread {
+	private ObjectOutputStream oos;
+	private OutputStream os;
+	private Socket socketCliente;
+	private Buffer buffer;
+	
+	
+	
+	public void run(){
+		while(true){
+		Object elemento = null;
+		try {
+			
+			elemento=buffer.take();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		send(elemento);
+		}
+	}
+	
+	
+	
+	private void send(Object elemento){
+		try {
+			oos.writeObject(elemento);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	
+	
+	public Transmisor(Buffer buffer,Socket socketCliente){
+		this.buffer=buffer;
+		this.setSocketCliente(socketCliente);
+		
+		try {
+			os = socketCliente.getOutputStream();
+			oos = new ObjectOutputStream(os);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	
+
+	public Buffer getBuffer() {
+		return buffer;
+	}
+	public void setBuffer(Buffer buffer) {
+		this.buffer = buffer;
+	}
+
+
+
+	public Socket getSocketCliente() {
+		return socketCliente;
+	}
+
+
+
+	public void setSocketCliente(Socket socketCliente) {
+		this.socketCliente = socketCliente;
+	}
+	
+	
+	
+}
+
