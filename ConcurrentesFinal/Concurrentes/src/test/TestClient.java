57af7989fea4db10024e95254dcc7efcacb0ee7f
vegasruffmurphy@gmail.com <vegasruffmurphy@gmail.com@7e0d7668-b102-ec16-678f-7dbf3825cd3e>
2013-07-25 02:59:42 +0000

    modificacion de los tests de comunicaciones

--- ConcurrentesFinal/Concurrentes/src/test/TestClient.java
+++ ConcurrentesFinal/Concurrentes/src/test/TestClient.java
@@ -3,6 +3,7 @@ import java.net.*;
 //import java.io.*;
 import Comunicacion.*;
 import Misiles.MisilEnemigo;
+import Misiles.Vector;
 
 public class TestClient {
 	
@@ -10,16 +11,20 @@ public class TestClient {
 
 public static void main(String[] args){
 Buffer colaRecibidos=new Buffer();
+Buffer colaEnviar=new Buffer();
 Receptor receptor;
 MisilEnemigo misil;
-
+Transmisor transmisor;
 	
 	try {    
 Socket s = new Socket("localhost",9000);  
+Socket s1= new Socket("localhost",9000);
 System.out.println("connected client");
 receptor=new Receptor(colaRecibidos,s);
+transmisor=new Transmisor(colaEnviar,s1);
 receptor.start();
-
+colaEnviar.put(new MisilEnemigo(new Vector(1,1,1),new Vector(1,2,3)));
+transmisor.start();
 while(true){
 	misil=(MisilEnemigo)colaRecibidos.take();
 	System.out.println(misil.getPosicion().getX());
--- ConcurrentesFinal/Concurrentes/src/test/TestServer.java
+++ ConcurrentesFinal/Concurrentes/src/test/TestServer.java
@@ -11,24 +11,28 @@ public class TestServer {
 
 public static void main(String[] args){
 Buffer colaEnviar=new Buffer();
+Buffer colaRec=new Buffer();
 Transmisor trans;
+Receptor rec;
 MisilEnemigo misil;
 	
 try {  
 ServerSocket ss = new ServerSocket(9000);  
 Socket s = ss.accept();  
+Socket s1 =ss.accept();
 System.out.println("connected");
 trans=new Transmisor(colaEnviar,s);
-
-
-
-
+rec=new Receptor(colaRec,s1);
 
 	misil=new MisilEnemigo(new Vector (0,0,0),new Vector (1,1,1));
 	colaEnviar.put(misil);
 	trans.start();
-	
-	
+	rec.start();
+	while(true){
+		misil=(MisilEnemigo)colaRec.take();
+		System.out.println(misil.getPosicion().getX());
+		
+	}
 
 
 }catch(Exception e){System.out.println(e);}  
