26c03f920627ea13ff3d75ba25a5c1fcb40e1321
vegasruffmurphy@gmail.com <vegasruffmurphy@gmail.com@7e0d7668-b102-ec16-678f-7dbf3825cd3e>
2013-07-26 19:01:10 +0000

    Se agrego la clase Monitor y HiloMisilDefensivo, tambien se realizo la ejecucion de; HiloMisilDefensivo desde el Servidor

---
+++ trunk/ConcurrentesFinal/Concurrentes/src/Monitor/Monitor.java
@@ -0,0 +1,19 @@
+package Monitor;
+
+import Misiles.MisilEnemigo;
+import Misiles.Vector;
+
+public class Monitor {
+
+	
+	
+	public Vector obtenerRecurso(MisilEnemigo misilEnemigo){
+		Vector posicionBase=null;
+		/*Calcular base mas cercana y devolver la posicion de la base disponible de la cual se procedera a lanzar el misil
+		 * Se puede utilizar los metodos provistos por AnalizadorTrayectoria para obtener el punto de interseccion con el cilindro
+		 */
+		
+		return posicionBase;
+		
+	}
+}
